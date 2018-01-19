1b43dc078273eee2d7effb5f77b110b274a1aa90
Insiyah <insiyahhajoori@gmail.com>
2018-01-19 21:07:02 +0530

    Fetch tweets and save as a json file for further analysis.

--- sentiments/mining.py
+++ sentiments/mining.py
@@ -1,16 +1,50 @@
 import tweepy
+import json
+import re
+#pip install nltk and install the package punkt of it. 
+from nltk.tokenize import word_tokenize
 
 # Consumer key and consumer secret are dummy. Add yours to make this work. I can't share mine publicily.
-consumer_key = 'HSsf2YGAw7f'
-consumer_secret = 'AHCKKukN4RBBIEDzBAUz'
-access_token = '3168794738-IhJujdhmAZ8mO'
-access_secret = 'zrVSvnG5cohErrLTlp5bL'
+consumer_key = 'HSsAw7f'
+consumer_secret = 'AHCKKukzBAUz'
+access_token = '31687947kmXBSZfcXHo9'
+access_secret = 'ZSfjlM3FzK'
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-publicTweets = tweepy.Cursor(api.user_timeline, id = "twitter").items(10)
+'''
+tweet = "Playin around with tweepy!"
+try:
+    if api.update_status(status=tweet):
+        print("Posted")
+except tweepy.error.TweepError as e:
+    print(e)
+'''
+
+user = api.me()
+print("Name:",user.name)
+print("Location:", user.location)
+print("Friends:",user.friends_count)
+
+def process_or_store(tweet, here):
+    data = json.dumps(tweet._json)
+    with open("DATA/"+ here + ".json","a") as f:
+        f.write(data)
+
+
+publicTweets = tweepy.Cursor(api.home_timeline).items(10)
+
 for tweet in publicTweets:
-    print(tweet.text)
+    process_or_store(tweet, "tweets")
+
+for friend in tweepy.Cursor(api.friends).items():
+    process_or_store(friend, "friends")
+
+tweet = 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
+print(word_tokenize(tweet))
+
+
