936c8183d3d436c049e6d6ef53279f8f1ad6dfd7
prachi-agrawal1179 <agrawalprachi1179@gmail.com>
2018-02-06 00:35:56 +0530

    Add some more stuffs

--- sentiments/app.py
+++ sentiments/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for, jsonify
+from flask import Flask, request, render_template, url_for, jsonify, flash
 import tweepy
 import json
 import pandas as pd
@@ -7,6 +7,7 @@ from collections import Counter
 import pygal
 
 app = Flask(__name__)
+app.secret_key = 'some secret key'
 #authentication
 consumer_key = 'HSsfiqvQcqKKJh6aRl2YGAw7f'
 consumer_secret = 'AHCKKukN4JAcOjlHarPsAf6MdVF3C1iwZaKUPo9RBBIEDzBAUz'
@@ -30,68 +31,73 @@ def data():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        name = request.form.get("tw_id")
-        publicTweets = tweepy.Cursor(api.user_timeline, id=name).items(50)
-        followers = tweepy.Cursor(api.followers,id=name).items(100)
+        try:
+            name = request.form.get("tw_id")
+            publicTweets = tweepy.Cursor(api.user_timeline, id=name).items(50)
+            followers = tweepy.Cursor(api.followers,id=name).items(100)
 
-        user = api.get_user(name)
-        screen_name =  user.screen_name
-        des = user.description
-        followers_c =  user.followers_count
-        status_c = user.statuses_count
+            user = api.get_user(name)
+            screen_name =  user.screen_name
+            des = user.description
+            followers_c =  user.followers_count
+            status_c = user.statuses_count
 
-        tweet_text = []
-        languages = []
-        f_loc = []
+            tweet_text = []
+            languages = []
+            f_loc = []
 
-        for x in publicTweets:
-            tweet_text.append(x.text)
-            languages.append(x.lang)
-        for f in followers:
-            if not f.location == '':
-                f_loc.append(f.location)
-        print(len(tweet_text))
-        process = Process()
-        tweet_text = process.preprocess(tweet_text)
+            for x in publicTweets:
+                tweet_text.append(x.text)
+                languages.append(x.lang)
+            for f in followers:
+                if not f.location == '':
+                    f_loc.append(f.location)
+            print(len(tweet_text))
+            process = Process()
+            tweet_text = process.preprocess(tweet_text)
 
-        analytics = process.analyze(tweet_text)
-        total = analytics[0] + analytics[1] + analytics[2]
-        analytics = list(map(lambda x: x * (100/total),list(analytics)))
-        pie_chart = pygal.Pie()
-        pie_chart.title = 'Sentiments analysis (in %)'
-        pie_chart.add('Positive', analytics[0])
-        pie_chart.add('Negative', analytics[1])
-        pie_chart.add('Swearing', analytics[2])
-        chart1 = pie_chart.render_data_uri()
+            analytics = process.analyze(tweet_text)
+            total = analytics[0] + analytics[1] + analytics[2]
+            analytics = list(map(lambda x: x * (100/total),list(analytics)))
+            pie_chart = pygal.Pie()
+            pie_chart.title = 'Sentiments analysis (in %)'
+            pie_chart.add('Positive', analytics[0])
+            pie_chart.add('Negative', analytics[1])
+            pie_chart.add('Swearing', analytics[2])
+            chart1 = pie_chart.render_data_uri()
 
-        count_lang = Counter()
-        count_lang.update(languages)
-        count_lang = count_lang.most_common(8)
-        sum = 0
-        for e in count_lang:
-            sum += e[1]
+            count_lang = Counter()
+            count_lang.update(languages)
+            count_lang = count_lang.most_common(8)
+            sum = 0
+            for e in count_lang:
+                sum += e[1]
 
-        pie_chart = pygal.Pie()
-        pie_chart.title = 'Languages used (in %)'
-        for e in count_lang:
-            if not e[0] == "und":
+            pie_chart = pygal.Pie()
+            pie_chart.title = 'Languages used (in %)'
+            for e in count_lang:
+                if not e[0] == "und":
+                    pie_chart.add(e[0], (e[1] * (100/sum)))
+            chart2 = pie_chart.render_data_uri()
+
+            count_loc = Counter()
+            count_loc.update(f_loc)
+            count_loc = count_loc.most_common(8)
+            sum = 0
+            for e in count_loc:
+                sum += e[1]
+
+            pie_chart = pygal.Pie()
+            pie_chart.title = 'Location of followers(in %)'
+            for e in count_loc:
                 pie_chart.add(e[0], (e[1] * (100/sum)))
-        chart2 = pie_chart.render_data_uri()
+            chart3 = pie_chart.render_data_uri()
+            return render_template("data.html", s_name = screen_name, des = des, f_c = followers_c, s_c = status_c, y = count_lang, z = count_loc, chart1 = chart1, chart2 = chart2, chart3 = chart3)
+        except:
+            flash("Oops! We missed somewhere. Try again later!")
+            return render_template("index.html")
 
-        count_loc = Counter()
-        count_loc.update(f_loc)
-        count_loc = count_loc.most_common(8)
-        sum = 0
-        for e in count_loc:
-            sum += e[1]
-
-        pie_chart = pygal.Pie()
-        pie_chart.title = 'Location of followers(in %)'
-        for e in count_loc:
-            pie_chart.add(e[0], (e[1] * (100/sum)))
-        chart3 = pie_chart.render_data_uri()
-
-        return render_template("data.html", s_name = screen_name, des = des, f_c = followers_c, s_c = status_c, y = count_lang, z = count_loc, chart1 = chart1, chart2 = chart2, chart3 = chart3)
+        
 
 
 if __name__ == "__main__":
